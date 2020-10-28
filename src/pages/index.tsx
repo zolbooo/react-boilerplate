bb5e46a9d60217380af44106cd8e34802f0f0f0e
Zolboobayar Gantumur <zolboobayar@nhs.edu.mn>
2020-10-28 11:57:08 +0800

    Add default page

---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import Head from 'next/head';
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+    </>
+  );
+}
